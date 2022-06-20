70e8c06d951c237803993be2553cc49c0e74f3f4
garcia-cyber <mukokogarciagx@gmail.com>
2022-06-20 15:12:54 -0400

    login admin disigner machine  fin

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-from flask import Flask,session,redirect,url_for, render_template,flash
+from flask import Flask,session,redirect,url_for, render_template,flash,request
 import mysql.connector as mysql
 import bcrypt as code
 
 school = Flask(__name__)
 
-@school.route('/')
+@school.route('/home',methods=['GET','POST'])
 def home():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        user   = request.form.get('email')
+        login  = request.form.get('password')
+        
+        
+        
     return render_template('home.html')
 
+"""
+*********************************************************************
+    partie administration
+    
+*********************************************************************
+""" 
 
-
-
-
-
-
+@school.route('/')
+@school.route('/admin')
+def admin():
+    return render_template('index.html')
 
 
 
 
 if __name__ == '__main__':
     school.run(debug=True)
+    
+    
+    
---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+const bo = document.querySelector('.boll');
+
+
+window.addEventListener("mousemove", (e) => {
+    bo.style.left = e.pageX + "px";
+    bo.style.top  = e.pageY + "px";
+});
