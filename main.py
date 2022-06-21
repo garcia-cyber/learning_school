5ce67e0bccdd15062d7f548ec04e853a9313f17d
garcia-cyber <mukokogarciagx@gmail.com>
2022-06-20 20:34:05 -0400

    je travaille sur la partie login administrateur thanks you

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import bcrypt as code
 school = Flask(__name__)
 school.secret_key = 'mukoko mbala gracia'
 
+"""
+*********************************************************************
+    partie de la connection avec mon data
+    
+*********************************************************************
+""" 
+
+sql = mysql.connect(host='localhost',user = 'linux',password = 'data',database = 'school')
+
+
+
 
 @school.route('/home',methods=['GET','POST'])
 def home():
@@ -14,6 +25,23 @@ def home():
         user   = request.form.get('email')
         login  = request.form.get('password')
         
+        cur = sql.cursor()
+        cur.execute("SELECT * FROM userAdmin where email_admin = %s AND password_admin = %s ",
+                    (user,login,))
+        
+        query = cur.fetchone()
+        b = query[1]
+        cur.close()
+        
+        
+        
+        if len(query) > 0:
+            session['nameAdm'] = query[1]
+            session['adminUserLogin'] = True
+            
+            
+            # session['username_admin'] = query['username_admin']
+            return redirect(url_for('admin'))
         
         
     return render_template('home.html')
@@ -28,7 +56,19 @@ def home():
 @school.route('/')
 @school.route('/admin')
 def admin():
-    return render_template('index.html')
+    
+    if 'nameAdm' in session:
+        v = session['nameAdm']
+        return render_template('index.html',user = session['nameAdm']) 
+    else:
+        return redirect(url_for('home'))
+
+
+@school.route('/deco')
+
+def deco():
+    session.clear()
+    return redirect(url_for('home'))
 
 
 
--- static/data/data.sql
+++ static/data/data.sql
@@ -7,10 +7,46 @@ use school;
 create table userAdmin(
     id_admin tinyint auto_increment primary key,
     username_admin varchar(50),
-    email_admin varchar(255),
+    email_admin varchar(100),
     password_admin varchar(255)
 );
 
--- create table parents 
+-- ajout du user et password par defaut
 
-create table 
+insert into userAdmin(username_admin,email_admin,password_admin)values('mukoko','mukokogarciagx@gmail.com','user');
+
+
+
+-- table commune 
+create table communes(
+    id_commune tinyint auto_increment primary key,
+    lib_commune varchar(25)
+);
+
+-- insertion de quelques communes 
+
+insert into communes(lib_commune)values("Matete"),("Limete"),("Lemba"),("N'djili")("Masina");
+
+-- create table parents  avec commune comme cle etranger
+
+create table parents(
+    id_parent smallint auto_increment primary key,
+    username_parent varchar(50),
+    email_parent varchar(100),
+    commune_parent tinyint,
+    adresse_parent varchar(150),
+    phone_parent varchar(15),
+    password_parent varchar(255)
+);
+
+
+
+
+
+
+-- add foreign key with all setting
+alter table parents add constraint fk_commune foreign key(commune_parent) references communes(id_commune)
+on delete set null  on update cascade;
+
+
+
